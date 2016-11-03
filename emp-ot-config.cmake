9c013e3410ed51084cc87002fabbb3dba159077e
Ubuntu <ubuntu@ip-172-31-6-191.ec2.internal>
2016-11-03 15:38:26 +0000

    use cmake files to locate

--- emp-ot-config.cmake
+++ emp-ot-config.cmake
@@ -1,6 +1,7 @@
 find_package(emp-tool)
 
-find_path(EMP-OT_INCLUDE_DIR emp-ot/emp-ot.h)
+
+find_path(EMP-OT_INCLUDE_DIR cmake/emp-ot-config.cmake)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EMP-OT DEFAULT_MSG EMP-OT_INCLUDE_DIR)
