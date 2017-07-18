121bbb8893642ed01e3b1102683dc4ba298c9e9b
Ubuntu <ubuntu@ip-172-31-25-110.ec2.internal>
2017-07-18 21:25:34 +0000

    make release default

--- test/mot.cpp
+++ test/mot.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char** argv) {
 	int port, party, length = 1<<22;
 	parse_party_and_port(argv, 2, &party, &port);
-	NetIO * io = new NetIO(party==ALICE ? nullptr:SERVER_IP, port);
+	NetIO * io = new NetIO(party==ALICE ? nullptr:"127.0.0.1", port);
 	cout <<"COOT\t"<<10000.0/test_ot<NetIO, OTCO>(io, party, 10000)*1e6<<" OTps"<<endl;
 	cout <<"Malicious OT Extension\t"<<double(length)/test_ot<NetIO, MOTExtension>(io, party, length)*1e6<<" OTps"<<endl;
 	cout <<"Malicious ROT Extension\t"<<double(length)/test_rot<NetIO, MOTExtension>(io, party, length)*1e6<<" OTps"<<endl;
