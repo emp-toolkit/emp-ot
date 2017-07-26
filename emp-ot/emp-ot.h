902d9ef7cb7b1e2f27c637de02929bd870179202
Xiao Wang <wangxiao1254@gmail.com>
2017-07-26 17:47:17 -0400

    missing files

---
+++ emp-ot/emp-ot.h
@@ -0,0 +1,17 @@
+/** @defgroup OT Oblivious Transfer
+*/
+
+#include "emp-ot/ot.h"
+#include "emp-ot/ideal.h"
+
+#include "emp-ot/co.h"
+#include "emp-ot/np.h"
+
+#include "emp-ot/shextension.h"
+#include "emp-ot/ot_extension.h"
+#include "emp-ot/mextension_kos.h"
+#include "emp-ot/mextension_alsz.h"
+
+template<typename IO>
+using MOTExtension = MOTExtension_KOS<IO>;
+//typedef MOTExtension_ALSZ MOTExtension;
