87ca705d102d3322551e50684a039467a4b041f7
Xiao Wang <wangxiao@cs.northwestern.edu>
2019-10-03 14:52:22 -0500

    minor

--- emp-ot/co.h
+++ emp-ot/co.h
@@ -7,48 +7,50 @@
   */
 namespace emp {
 template<typename IO>
-class OTCO: public OT<OTCO<IO>> { 
-
-public:
+class OTCO: public OT<OTCO<IO>> { public:
 	IO* io;
-	Group *G;
+	Group *G = nullptr;
+	bool delete_G = true;
 	OTCO(IO* io, Group * _G = nullptr) {
 		this->io = io;
 		if (_G == nullptr)
 			G = new Group();
-		else
+		else {
 			G = _G;
+			delete_G = false;
+		}
+	}
+	~OTCO() {
+		if (delete_G)
+			delete G;
 	}
 
 	void send_impl(const block* data0, const block* data1, int length) {
 		BigInt a;
 		Point A, AaInv;
+		block res[2];
 		Point * B = new Point[length];
 		Point * BA = new Point[length];
+		for (int i = 0; i < length; ++i) {
+			B[i] = Point(G);
+			BA[i] = Point(G);
+		}
 
 		G->get_rand_bn(a);
-		G->init(A);
-		G->init(AaInv);
-		for(int i = 0; i < length; ++i) {
-			G->init(B[i]);
-			G->init(BA[i]);
-		}
-		
-		block res[2];
-		G->mul_gen(A, a);
-		G->mul(AaInv, A, a);
-		G->inv(AaInv, AaInv);
-		io->send_pt(G, &A);
+		A = G->mul_gen(a);//g^a
+		AaInv = A.mul(a);//g^(a^2)
+		AaInv = AaInv.inv();//g^(-a^2)
+		io->send_pt(&A);
 
 		for(int i = 0; i < length; ++i) {
-			io->recv_pt(G, B + i);
-			G->mul(B[i], B[i], a);
-			G->add(BA[i], B[i], AaInv);
+			io->recv_pt(&B[i]);
+			B[i] = B[i].mul(a);
+			BA[i] = B[i].add(AaInv);
 		}
 
 		for(int i = 0; i < length; ++i) {
-			res[0] = Hash::KDF(G, B[i], i);
-			res[1] = Hash::KDF(G, BA[i], i);
+			res[0] = Hash::KDF(B[i], i);
+			res[1] = Hash::KDF(BA[i], i);
 			res[0] = xorBlocks(res[0], data0[i]);
 			res[1] = xorBlocks(res[1], data1[i]);
 
@@ -62,35 +64,37 @@ public:
 	void recv_impl(block* data, const bool* b, int length) {
 		BigInt * bb = new BigInt[length];
 		Point * B = new Point[length];
-		Point A;
-		G->init(A);
+		Point * As = new Point[length];
+		Point A(G);
 		for(int i = 0; i < length; ++i) {
-			G->init(B[i]);
 			G->get_rand_bn(bb[i]);
-			G->mul_gen(B[i], bb[i]);
+			B[i] = G->mul_gen(bb[i]);
+			As[i] = Point(G);
 		}
 
-		io->recv_pt(G, &A);
+		io->recv_pt(&A);
 		for(int i = 0; i < length; ++i)
 			if (b[i]) 
-				G->add(B[i], A, B[i]);
+				B[i] = B[i].add(A);
 
-		io->send_pt(G, B, length);io->flush();
+		for (int i = 0; i < length; ++i)
+			io->send_pt(&B[i]);
+
 		for(int i = 0; i < length; ++i)
-			G->mul(A, A, bb[i]);
+			As[i] = A.mul(bb[i]);
 
 		block res[2];
 		for(int i = 0; i < length; ++i) {
 			io->recv_data(res, 2*sizeof(block));
-			data[i] = Hash::KDF(G, A, i);
+			data[i] = Hash::KDF(As[i], i);
 			if(b[i])
 				data[i] = xorBlocks(data[i], res[1]);
 			else
 				data[i] = xorBlocks(data[i], res[0]);
-	}
+		}
 		
-	delete[] bb;
-	delete[] B;
+		delete[] bb;
+		delete[] B;
 	}
 };
   /**@}*/
