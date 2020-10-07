108dd395500660cab3e874753219d7b4b0f8c6fb
Xiao Wang <wangxiao1254@gmail.com>
2020-10-07 13:48:39 -0500

    Update constants.h

--- emp-ot/ferret/constants.h
+++ emp-ot/ferret/constants.h
@@ -5,8 +5,8 @@ namespace emp {
 std::string PRE_OT_DATA_REG_SEND_FILE = "./data/pre_ot_data_reg_send";
 std::string PRE_OT_DATA_REG_RECV_FILE = "./data/pre_ot_data_reg_recv";
 
-const static int N_REG = 10608640;
-const static int T_REG = 1295;
+static const static int N_REG = 10608640;
+static const static int T_REG = 1295;
 const static int K_REG = 589824;
 const static int BIN_SZ_REG = 13;
 const static int N_PRE_REG = 649728;
