6d600c04420375f5c881973836d3eec4311a44b9
Xiao Wang <wangxiao1254@gmail.com>
2021-02-10 21:17:36 -0600

    add GitHub Action
    * Create x86.yml
    
    * Update x86.yml
    
    * Update x86.yml
    
    * update test
    
    * update test

--- test/ot.cpp
+++ test/ot.cpp
@@ -24,24 +24,19 @@ int main(int argc, char** argv) {
 	cout <<"Active IKNP COT\t"<<double(length)/test_cot<IKNP<NetIO>>(iknp, io, party, length)*1e6<<" OTps"<<endl;
 	cout <<"Active IKNP ROT\t"<<double(length)/test_rot<IKNP<NetIO>>(iknp, io, party, length)*1e6<<" OTps"<<endl;
 	delete iknp;
+
+	FerretCOT<NetIO> * ferretcot = new FerretCOT<NetIO>(party, threads, &io, false);
+	cout <<"Passive FERRET OT\t"<<double(length)/test_ot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	cout <<"Passive FERRET COT\t"<<double(length)/test_cot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	cout <<"Passive FERRET ROT\t"<<double(length)/test_rot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	delete ferretcot;
+	ferretcot = new FerretCOT<NetIO>(party, threads, &io, true);
+	cout <<"Active FERRET OT\t"<<double(length)/test_ot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	cout <<"Active FERRET COT\t"<<double(length)/test_cot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	cout <<"Active FERRET ROT\t"<<double(length)/test_rot<FerretCOT<NetIO>>(ferretcot, io, party, length)*1e6<<" OTps"<<endl;
+	delete ferretcot;
+
+
 	delete io;
-
-	NetIO* ios[threads];
-	for(int i = 0; i < threads; ++i)
-		ios[i] = new NetIO(party == ALICE?nullptr:"127.0.0.1",port+i);
-	FerretCOT<NetIO> * ferretcot = new FerretCOT<NetIO>(party, threads, ios, false);
-	cout <<"Passive FERRET OT\t"<<double(length)/test_ot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	cout <<"Passive FERRET COT\t"<<double(length)/test_cot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	cout <<"Passive FERRET ROT\t"<<double(length)/test_rot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	delete ferretcot;
-	ferretcot = new FerretCOT<NetIO>(party, threads, ios, true);
-	cout <<"Active FERRET OT\t"<<double(length)/test_ot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	cout <<"Active FERRET COT\t"<<double(length)/test_cot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	cout <<"Active FERRET ROT\t"<<double(length)/test_rot<FerretCOT<NetIO>>(ferretcot, ios[0], party, length)*1e6<<" OTps"<<endl;
-	delete ferretcot;
-
-
-	for(int i = 0; i < threads; ++i)
-		delete ios[i];
 }
 
