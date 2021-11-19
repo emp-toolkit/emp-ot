84d98b281318d1af82b3c66a04ce79cf2ccca6f3
Xiao Wang <wangxiao1254@gmail.com>
2021-11-19 16:14:18 -0600

    fix include order (#64)

--- cmake/emp-ot-config.cmake
+++ cmake/emp-ot-config.cmake
@@ -7,6 +7,6 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(emp-ot DEFAULT_MSG EMP-OT_INCLUDE_DIR)
 
 if(EMP-OT_FOUND)
-	set(EMP-OT_INCLUDE_DIRS ${EMP-OT_INCLUDE_DIR} ${EMP-TOOL_INCLUDE_DIRS})
+	set(EMP-OT_INCLUDE_DIRS ${EMP-TOOL_INCLUDE_DIRS} ${EMP-OT_INCLUDE_DIR})
 	set(EMP-OT_LIBRARIES ${EMP-TOOL_LIBRARIES})
 endif()
